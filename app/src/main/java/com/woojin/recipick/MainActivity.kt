54befe03626a568429189a4a4974ce18d47a5841
woojin-tionnet <dldnwls0115@tionnet.com>
2025-05-23 13:11:32 +0900

    Design - 앱바 수정
    1. title이 중앙 정렬이 되도록 TopAppBar대신 CenterAlignedTopAppBar사용
    2. strings.xml 적용

--- app/src/main/java/com/woojin/recipick/MainActivity.kt
+++ app/src/main/java/com/woojin/recipick/MainActivity.kt
@@ -4,20 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.woojin.recipick.ui.theme.RecipickTheme
 import com.woojin.recipick.ui.theme.mainColor
@@ -43,19 +43,17 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyTopAppBar() {
-    Column {
-        TopAppBar(
-            title = { Text("My Recipes") },
-            actions = {
-                IconButton(onClick = {}) {
-                    Icon(imageVector = Icons.Filled.Add, contentDescription = "검색")
-                }
-            },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = mainColor
-            )
+    CenterAlignedTopAppBar(
+        title = { Text(stringResource(R.string.main_title)) },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = "검색")
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = mainColor
         )
-    }
+    )
 }
 
 @Preview(showBackground = true)
