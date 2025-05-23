9030c9c8d085dd1ca8c9182dee61e4460ecd9f52
woojin-tionnet <dldnwls0115@tionnet.com>
2025-05-23 13:06:32 +0900

    Feat - 메인 상단 앱바 추가
    1. 메인 화면 상단 앱바 추가

--- app/src/main/java/com/woojin/recipick/MainActivity.kt
+++ app/src/main/java/com/woojin/recipick/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.woojin.recipick.ui.theme.RecipickTheme
+import com.woojin.recipick.ui.theme.mainColor
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +28,45 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RecipickTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { MyTopAppBar() }
+                ) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
+                    MyTopAppBar()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyTopAppBar() {
+    Column {
+        TopAppBar(
+            title = { Text("My Recipes") },
+            actions = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Filled.Add, contentDescription = "검색")
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = mainColor
+            )
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppBarPreview() {
     RecipickTheme {
-        Greeting("Android")
+        MyTopAppBar()
     }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier) {
+
 }
