63d5266857135f802a38c0e7f0dd4a124a5f30bc
woojin-tionnet <dldnwls0115@tionnet.com>
2025-05-23 16:59:26 +0900

    Feat - NavHost사용을 위한 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,7 @@ dependencies {
     //Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+
+    //네비게이션
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 }
