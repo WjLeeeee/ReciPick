ce52bfb7e0c40e083c33a4f2cd39333cafec9de4
woojin-tionnet <dldnwls0115@tionnet.com>
2025-06-04 14:23:50 +0900

    Feat - Room DB 사용을 위한 설정 추가
    1. RecipeTable 이름의 레시피 Entitiy생성
    2. 아이템 삽입, 삭제 및 불러오기 DAO생성
    3. ListConverter적용 및 Hilt 적용

---
+++ app/src/main/java/com/woojin/recipick/data/local/dao/RecipeDao.kt
@@ -0,0 +1,19 @@
+package com.woojin.recipick.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.woojin.recipick.data.local.entity.RecipeEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipeDao {
+    @Insert
+    suspend fun insert(recipe: RecipeEntity)
+
+    @Query("DELETE FROM RecipeTable WHERE id = :recipeId")
+    suspend fun delete(recipeId: Int)
+
+    @Query("SELECT * FROM RecipeTable ORDER BY id DESC")
+    fun getAll(): Flow<List<RecipeEntity>>
+}
---
+++ app/src/main/java/com/woojin/recipick/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.woojin.recipick.di
+
+import android.content.Context
+import androidx.room.Room
+import com.woojin.recipick.data.local.AppDatabase
+import com.woojin.recipick.data.local.dao.RecipeDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            "recipe_database"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRecipeDao(appDatabase: AppDatabase): RecipeDao {
+        return appDatabase.recipeDao()
+    }
+}
