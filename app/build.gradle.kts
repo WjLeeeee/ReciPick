40c2a6019f77895a18ba523d11acc6f5c1e18eaa
woojin-tionnet <dldnwls0115@tionnet.com>
2025-06-04 14:21:57 +0900

    Feat - 의존성 추가
    1. Room 라이브러리 사용을 위한 의존성 추가
    2. Room에 List형식의 데이터가 추가되서, Gson을 사용해 Converter를 만들어 주기 위해 Gson라이브러리 추가
    3. 코루팀 및 viewModel 델리게이트 사용을 위한 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,22 @@ dependencies {
     //Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0") //hiltViewModel 델리게이트 사용을 위한 추가
 
     //네비게이션
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    //Room
+    val roomVersion = "2.4.2"
+    implementation("androidx.room:room-runtime:$roomVersion") //기본
+    ksp("androidx.room:room-compiler:$roomVersion") //또는 kapt
+    implementation("androidx.room:room-ktx:$roomVersion") //Flow지원을 위해서
+
+    //Coroutines
+    val coroutineVersion = "1.7.3"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+
+    //GSON
+    implementation("com.google.code.gson:gson:2.10.1")
 }
