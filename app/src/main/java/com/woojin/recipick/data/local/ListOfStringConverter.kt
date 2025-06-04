cdc7d0b9901516ac37810af2867e85c0b95ed300
woojin-tionnet <dldnwls0115@tionnet.com>
2025-06-04 14:22:28 +0900

    Feat - 리스트 Converter 생성
    1. Room DB에 List형식의 데이터 저장하기 위한 컨버터 생성

---
+++ app/src/main/java/com/woojin/recipick/data/local/ListOfStringConverter.kt
@@ -0,0 +1,27 @@
+package com.woojin.recipick.data.local
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+
+class ListOfStringConverter {
+    private val gson = Gson()
+
+    @TypeConverter
+    fun fromString(value: String?): List<String>? {
+        if (value == null) {
+            return null
+        }
+        val listType = object : TypeToken<List<String>>() {}.type
+        return gson.fromJson(value, listType)
+    }
+
+    @TypeConverter
+    fun fromList(list: List<String>?): String? {
+        if (list == null) {
+            return null
+        }
+        return gson.toJson(list)
+    }
+}
