88aeeee2658c6bd84c0944dbfe0b99f85e6b081f
woojin-tionnet <dldnwls0115@tionnet.com>
2025-06-20 13:35:43 +0900

    Comment - 유즈케이스 주석 추가

--- app/src/main/java/com/woojin/recipick/domain/usecase/UpdateRecipeUseCase.kt
+++ app/src/main/java/com/woojin/recipick/domain/usecase/UpdateRecipeUseCase.kt
@@ -4,6 +4,7 @@ import com.woojin.recipick.data.local.entity.RecipeEntity
 import com.woojin.recipick.domain.repository.RecipeRepository
 import javax.inject.Inject
 
+/** 수정된 레시피 저장 하는 UseCase */
 class UpdateRecipeUseCase @Inject constructor(
     private val recipeRepository: RecipeRepository
 ) {
